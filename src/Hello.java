8e62e063e0ebe737ebf24222001039f7eedf052d
박장현 <janghyn97@gmail.com>
2026-08-25 16:12:00 +0900

    feat: 시도 횟수 표시 및 출력 문구 정리

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.printf("안녕하세요 박장현입니다.");
+        System.out.printf("안녕하세요 박장현입니다");
         System.out.printf("자바 버전 : " + System.getProperty("java.version"));
 
     }
--- src/NumberGame.java
+++ src/NumberGame.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
 public class NumberGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int answer = (int) (Math.random() * 100) + 1;
+        int number = 0;
+
+        while (true) {
+            System.out.println("정답은? (1~100)");
+            int a = sc.nextInt();
+
+            if (a < 1 || a > 100) {
+                System.out.println("다시 입력 하세요");
+                continue;
+            }
+
+            number++;
+
+            if (a == answer) {
+                System.out.println("정답입니다! 정답은 "  + number + "번에 맞추셨습니다.");
+                break;
+            } else if (a > answer) {
+                System.out.println("정답보다 큽니다");
+            } else {
+                System.out.println("정답보다 작습니다");
+            }
+        }
+
+        sc.close();
+    }
 }
