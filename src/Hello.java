b676477a62bfef53a565c37a689fe69afe0cc751
박장현 <janghyn97@gmail.com>
2026-08-26 15:18:32 +0900

    feat: 인사말 영어로

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.printf("Hello, Java World");
+        System.out.printf("Hellod world?");
         System.out.printf("자바 버전 : " + System.getProperty("java.version"));
 
     }
